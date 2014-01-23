1ac05c836178a6297dfc5320d0937306d7a8e82c
jeswinjames24 <jeswinjames24@gmail.com>
2014-01-23 08:32:56 +0000

    Added login with Devise

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module ProjectnNl
     config.assets.enabled = true
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
     config.assets.precompile += %w( .svg .eot .woff .ttf )
+    
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 ProjectnNl::Application.routes.draw do
+  devise_for :users
   resources :tests
-
+  
+  root :to => 'tests#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
